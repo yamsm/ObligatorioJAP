05aa64fc11c1623305acbccf23723063895204c1
yamsm <49851443+yamsm@users.noreply.github.com>
2019-10-02 20:00:14 -0300

    Modificacion

--- entrega1/js/category-info.js
+++ entrega1/js/category-info.js
@@ -21,7 +21,8 @@ function showImagesGallery(array){
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+//elementos HTML presentes .
+// HOLAAAAAAAAA
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CATEGORY_INFO_URL).then(function(resultObj){
         if (resultObj.status === "ok")
