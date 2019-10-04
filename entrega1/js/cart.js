6493083eafb42b06945673edc83e6873cb2256d4
yamsm <49851443+yamsm@users.noreply.github.com>
2019-10-04 11:54:49 -0300

    update

--- entrega1/js/cart.js
+++ entrega1/js/cart.js
@@ -29,6 +29,8 @@ function showArticles(articles){
 
 }
 
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
