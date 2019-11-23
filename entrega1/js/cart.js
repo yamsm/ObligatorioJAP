d6aa5e67acffedb6acd9a4ac294ca29bed749c8f
yamsm <49851443+yamsm@users.noreply.github.com>
2019-11-23 19:32:53 -0300

    .

--- entrega1/js/cart.js
+++ entrega1/js/cart.js
@@ -6,31 +6,42 @@ let total = 0;
 let paymentTypeSelected = false;
 const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
 const BANKING_PAYMENT = "Transferencia bancaria";
+let PERCENTAGE_SYMBOL = '%';
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
+  let subtotalToShow = productCurrency + ` ` + subtotal;
+  let shippingPercentageToShow = Math.round((shippingPercentage * 100)) + PERCENTAGE_SYMBOL;
+  let shippingTotal = Math.round(subtotal * shippingPercentage * 100) / 100;
+  total = subtotal + shippingTotal;
+  let shippingTotalToShow = productCurrency + ` ` + shippingTotal;
+  let totalCostToShow = productCurrency + ` ` + total;
 
+  document.getElementById("productCostSubtotal").innerHTML = subtotalToShow;
+  document.getElementById("porcentajeEnvio").innerHTML = shippingPercentageToShow;
+  document.getElementById("costoEnvio").innerHTML = shippingTotalToShow;
+  document.getElementById("totalCost").innerHTML = totalCostToShow;
 }
 
 function updateSubtotal(){
   let productCountHTML = document.getElementById("articleCount").value;
 
-  subtotal = Math.round(productCountHTML * productUnitCost * 100) / 100
+  subtotal = Math.round(productCountHTML * productUnitCost * 100) / 100;
   let subtotalToShow = productCurrency + ` ` + subtotal;
 
   document.getElementById("articleSubtotal").innerHTML = subtotalToShow;
   document.getElementById("productCostSubtotal").innerHTML = subtotalToShow;
-
-
 }
 
 function showPaymentTypeNotSelected(){
-
+  let validacionFormaDePago = document.getElementById("validacionFormaDePago");
+  validacionFormaDePago.classList.add('d-block');
 }
 
 function hidePaymentTypeNotSelected(){
-
+  let validacionFormaDePago = document.getElementById("validacionFormaDePago");
+  validacionFormaDePago.classList.remove('d-block');
 }
 
 function showArticles(articles){
@@ -50,6 +61,7 @@ function showArticles(articles){
   document.getElementById("articleSubtotal").innerHTML = productCurrency + ` ` + subtotal;
   document.getElementById("productCostSubtotal").innerHTML = productCurrency + ` ` + subtotal;
 
+  updateTotalCosts()
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -59,6 +71,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 
   document.getElementById("articleCount").addEventListener("change", function(){
       updateSubtotal();
+      updateTotalCosts()
   });
 
   getJSONData(CART_INFO_URL).then(function(resultObj){
@@ -67,9 +80,95 @@ document.addEventListener("DOMContentLoaded", function(e){
       articles = resultObj.data.articles;
       showArticles(articles);
     }
-
   });
 
+  document.getElementById("premiumradio").addEventListener("change", function(){
+      shippingPercentage = 0.15;
+      updateTotalCosts();
+  });
 
+  document.getElementById("expressradio").addEventListener("change", function(){
+      shippingPercentage = 0.07;
+      updateTotalCosts();
+  });
 
+  document.getElementById("standardradio").addEventListener("change", function(){
+      shippingPercentage = 0.05;
+      updateTotalCosts();
+  });
+
+  document.getElementById("creditCardPaymentRadio").addEventListener("change", function(){
+    paymentTypeSelected = CREDIT_CARD_PAYMENT;
+  });
+
+  document.getElementById("bankingRadio").addEventListener("change", function(){
+      paymentTypeSelected = BANKING_PAYMENT;
+  });
+
+  var cartForm = document.getElementById("cartForm");
+
+  cartForm.addEventListener("submit", function(e){
+
+    let infoMissing = false;
+    let creditCardNumberInput = document.getElementById("creditCardNumber");
+    let creditCardSecurityCodeInput = document.getElementById("creditCardSecurityCode");
+    let dueDateInput = document.getElementById("dueDate");
+    let bankAccountNumberInput = document.getElementById("bankAccountNumber");
+    creditCardNumberInput.classList.remove('is-invalid');
+    creditCardSecurityCodeInput.classList.remove('is-invalid');
+    dueDate.classList.remove('is-invalid');
+    bankAccountNumberInput.classList.remove('is-invalid');
+    hidePaymentTypeNotSelected();
+
+    if (paymentTypeSelected===false)
+    {
+      showPaymentTypeNotSelected();
+      infoMissing = true;
+    }
+    if (paymentTypeSelected === BANKING_PAYMENT){
+      if (bankAccountNumberInput.value === ""){
+        showPaymentTypeNotSelected();
+        bankAccountNumberInput.classList.add('is-invalid');
+        infoMissing = true;
+      }
+    }
+    if (paymentTypeSelected === CREDIT_CARD_PAYMENT){
+      if(creditCardNumberInput.value === ""){
+        showPaymentTypeNotSelected();
+        creditCardNumberInput.classList.add('is-invalid');
+        infoMissing = true;
+      }
+      if(creditCardSecurityCodeInput.value === ""){
+        showPaymentTypeNotSelected();
+        creditCardSecurityCodeInput.classList.add('is-invalid');
+        infoMissing = true;
+      }
+      if(dueDateInput.value === ""){
+        showPaymentTypeNotSelected();
+        dueDateInput.classList.add('is-invalid');
+        infoMissing = true;
+      }
+    }
+
+    if(!infoMissing)
+    {
+        getJSONData(CART_BUY_URL).then(function(resultObj){
+            let msgToShowHTML = document.getElementById("resultSpan");
+            let msgToShow = "";
+            if (resultObj.status === 'ok')
+            {
+                msgToShow = resultObj.data.msg;
+            }
+            else if (resultObj.status === 'error')
+            {
+                msgToShow = ERROR_MSG;
+            }
+
+            bootbox.alert(msgToShow, null);
+        });
+    }
+
+     if (e.preventDefault) e.preventDefault();
+           return false;
+  });
 });
